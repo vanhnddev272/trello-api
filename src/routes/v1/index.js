85885649d730d4214d35333e9bfd4f20aa0e844c
Viet Anh <vanhhavesomuchcute@gmail.com>
2023-12-10 03:38:17 +0700

    Add basic routes.

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,14 @@
-//
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    message: 'API v1 routes'
+  })
+})
+
+router.use('/boards', boardRoutes)
+
+export const API_V1 = router
--- src/server.js
+++ src/server.js
@@ -2,17 +2,14 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CLOSE_DB, CONNECT_DB, GET_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { API_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
 
-  app.get('/', async (req, res) => {
-    res.status(200).json({
-      data: await GET_DB().listCollections().toArray()
-    })
-  })
+  app.use('/v1', API_V1)
 
-  app.listen(() => {
+  app.listen(env.APP_PORT, () => {
     // eslint-disable-next-line no-console
     console.log(`Hello guys, I am running at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
   })
