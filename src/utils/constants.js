f2a17d2f3a01a4ae6f314bf4848e3584be5756d3
louis7552912 <louis@movegpt.io>
2025-01-02 16:30:36 +0700

    edit whitelist url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const WHITELIST_DOMAINS = [
-  // 'http://localhost:5173'
-  'https://trello-app-inky.vercel.app'
+  'http://localhost:5173',
+  'https://trello-app-inky.vercel.app',
+  'https://trello-api-3u3x.onrender.com/'
 ]
 
 export const BOARD_TYPE = {
